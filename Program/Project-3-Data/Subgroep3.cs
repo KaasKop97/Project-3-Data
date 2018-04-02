c7ef0159e8cbc82bdb483b48c5acc166d654d4b4
Yowhzie <jm_libier@hotmail.com>
2018-04-02 22:32:09 +0200

    uiteindelijke versie

--- Program/Project-3-Data/Subgroep3.cs
+++ Program/Project-3-Data/Subgroep3.cs
@@ -81,7 +81,32 @@ namespace Project_3
 
         private void button3_Click(object sender, EventArgs e)
         {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            conn();
+            String query = "Select sum(Games.owners)/10000 as owners, sum(Genres.users)/10000 as total_users from Games, Genres";
 
+            MySqlCommand cmd = new MySqlCommand(query, DBcon.dbConn);
+            DBcon.dbConn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            Dictionary<string, int> ExceptionMessages = new Dictionary<string, int>();
+
+            while (reader.Read())
+            {
+                //int total_users = reader.GetInt32("total_users");
+                //int owners = reader.GetInt32("owners");
+                ExceptionMessages.Add("Total users", reader.GetInt32("total_users"));
+                ExceptionMessages.Add("Owners", reader.GetInt32("owners"));
+                chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+                
+            }
+            foreach (KeyValuePair<string, int> exception in ExceptionMessages)
+                chart1.Series[0].Points.AddXY(exception.Key, exception.Value);
+
+            reader.Close();
+            DBcon.dbConn.Close();
         }
 
         public void conn()
