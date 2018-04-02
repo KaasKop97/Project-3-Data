c9a1cf940b35d980cb660e1bef8173e9c567ecb2
Yowhzie <jm_libier@hotmail.com>
2018-04-02 20:48:07 +0200

    nog niet af

--- Program/Project-3-Data/Subgroep3.Designer.cs
+++ Program/Project-3-Data/Subgroep3.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Game count by genre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Game count by corresponding amount of genres";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Owners out of total users";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(209, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 4.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.Legend = "Legend1";
+            series1.Name = "Game count";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(832, 518);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
             // 
             // Subgroep3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Subgroep3";
             this.Text = "Subgroep3";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Program/Project-3-Data/Subgroep3.cs
+++ Program/Project-3-Data/Subgroep3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Project_3
 {
@@ -15,6 +16,96 @@ namespace Project_3
         public Subgroep3()
         {
             InitializeComponent();
+            DBcon.InitializeDB();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            conn();
+            String query = "SELECT Genres.genre, count(Games.game) AS gamecount_genre FROM Genres_per_game INNER JOIN Genres ON(Genres.genre_id = Genres_per_game.genre_id) INNER JOIN Games ON(Games.game_id = Genres_per_game.game_id) WHERE Genres.genre_id = Genres_per_game.genre_id GROUP BY Genres.genre";
+
+            MySqlCommand cmd = new MySqlCommand(query, DBcon.dbConn);
+            DBcon.dbConn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            Dictionary<string, int> ExceptionMessages = new Dictionary<string, int>();
+
+            while (reader.Read())
+            {
+                ExceptionMessages.Add(reader.GetString("genre"),reader.GetInt32("gamecount_genre"));
+                chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+                chart1.ChartAreas["ChartArea1"].AxisY.Interval = 5;
+                
+            }
+            foreach (KeyValuePair<string, int> exception in ExceptionMessages)
+                chart1.Series[0].Points.AddXY(exception.Key, exception.Value);
+
+            reader.Close();
+            DBcon.dbConn.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            conn();
+            String query = "Select nr_of_genres, count(game_id) as nr_of_games From(Select game_id, count(genre_id) as nr_of_genres from Genres_per_game group by game_id order by game_id) as nr_genres_per_game group by nr_of_genres order by nr_of_genres";
+
+            MySqlCommand cmd = new MySqlCommand(query, DBcon.dbConn);
+            DBcon.dbConn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            Dictionary<string, int> ExceptionMessages = new Dictionary<string, int>();
+
+            while (reader.Read())
+            {
+                ExceptionMessages.Add(reader.GetString("nr_of_genres"), reader.GetInt32("nr_of_games"));
+                chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+                chart1.ChartAreas["ChartArea1"].AxisY.Interval = 2;
+                chart1.ChartAreas["ChartArea1"].AxisX.Title = "Number of genres";
+
+            }
+            foreach (KeyValuePair<string, int> exception in ExceptionMessages)
+                chart1.Series[0].Points.AddXY(exception.Key, exception.Value);
+
+            reader.Close();
+            DBcon.dbConn.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void conn()
+        {
+            String query = "SELECT Genres.genre, count(Games.game) AS gamecount_genre FROM Genres_per_game INNER JOIN Genres ON(Genres.genre_id = Genres_per_game.genre_id) INNER JOIN Games ON(Games.game_id = Genres_per_game.game_id) WHERE Genres.genre_id = Genres_per_game.genre_id GROUP BY Genres.genre";
+            //select from table games;
+
+            MySqlCommand cmd = new MySqlCommand(query, DBcon.dbConn);
+
+            DBcon.dbConn.Open();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                //chart1.Series["Genre"].Points.Add(reader.GetString("Genre"));
+                //chart1.Series["Game"].Points.Add(reader.GetString("Game"));
+
+            }
+
+            reader.Close();
+
+            DBcon.dbConn.Close();
         }
     }
 }
